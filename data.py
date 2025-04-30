ee3bbb70e92696f8d2589b23cb7d0007059fe5a6
nida maulida <nnidamaulida022@gmail.com>
2025-04-30 11:30:40 -0200

    Update Requirement

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 def load_data():
-    df = pd.read_csv("dataset\covid_19_indonesia_time_series_all.csv")
+    df = pd.read_csv("covid_19_indonesia_time_series_all.csv")
     df =df[df['Location'] != 'Indonesia']
     return df
 
